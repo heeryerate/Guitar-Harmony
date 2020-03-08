d0ae1072198354d6af172f8dd2b9f167699535b0
Xi He <heeryerate@gmail.com>
2020-03-07 20:23:37 -0500

    figure out pypi upload

--- guitarHarmony/__init__.py
+++ guitarHarmony/__init__.py
@@ -2,4 +2,15 @@
 # @Author: Xi He
 # @Date:   2017-09-17 09:13:29
 # @Last Modified by:   Xi He
-# @Last Modified time: 2020-03-07 12:13:15
+# @Last Modified time: 2020-03-07 20:14:17
+
+__all__ = [
+			'note',
+			'interval',
+			'chord',
+			'scale',
+			'progression',
+			'style',
+			'exercise'
+	]
+from guitarHarmony import *
